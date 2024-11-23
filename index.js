ca264cfa5e5324d44a3cf482e665a59c3e796e38
Jonathan Kong <jonathankong677@gmail.com>
2024-11-23 14:55:42 -0600

    c

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+    'https://peptalk-navy.web.app',
+    'http://localhost:5173' // For local testing
+];
+
 const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
